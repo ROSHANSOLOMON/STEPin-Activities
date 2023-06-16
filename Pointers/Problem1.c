873097fd505566cc1fd31cb01b4e148b035b644d
Roshan Solomon <93530703+ROSHANSOLOMON@users.noreply.github.com>
2023-06-16 19:14:57 +0530

    Create Problem1.c
    Write a program to print exponent part of double variable in hexadecimal and binary format.
    lets say we have x = 0.7, the exponent in hex is 0x3FE and 0b01111111110
    You may use the below link to validate you output
    https://baseconvert.com/ieee-754-floating-point
    
    Topics to be covered
    - Pointers
    - Bitwise Operators

---
+++ Pointers/Problem1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void printExponent(double num) {
+    unsigned long long *ptr = (unsigned long long *)&num;
+    unsigned long long exponent = (*ptr & 0x7FF0000000000000) >> 52;
+    
+    printf("Exponent in hexadecimal: 0x%llx\n", exponent);
+    
+    printf("Exponent in binary: 0b");
+    for (int i = 11; i >= 0; i--) {
+        printf("%llu", (exponent >> i) & 1);
+    }
+    printf("\n");
+}
+
+int main() {
+    double x = 0.7;
+    printExponent(x);
+
+    return 0;
+}
