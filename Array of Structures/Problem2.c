c44673f2f983a7dc4d24bd6b447b869cbb391789
Roshan Solomon <93530703+ROSHANSOLOMON@users.noreply.github.com>
2023-06-23 19:00:59 +0530

    Create Problem2.c
    Write a function to initialize all members in the above array of structures

---
+++ Array of Structures/Problem2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+void initializeStudents(struct Student* students, int size) {
+    for (int i = 0; i < size; ++i) {
+        students[i].rollno = 0;
+        strcpy(students[i].name, "");
+        students[i].marks = 0.0;
+    }
+}
+
+int main() {
+    int size;
+    printf("Enter the number of students: ");
+    scanf("%d", &size);
+    getchar();  // Consume newline character
+
+    struct Student* students = (struct Student*)malloc(size * sizeof(struct Student));
+
+    initializeStudents(students, size);
+
+    // Display the initialized array of structures
+    for (int i = 0; i < size; ++i) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("\n");
+    }
+
+    free(students);
+
+    return 0;
+}
